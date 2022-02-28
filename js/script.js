3033e772ba5932b945c8897c4be9c10246591c43
Hossain-Sajjad <sunshinems58.com>
2022-03-01 04:03:45 +0600

    detail section api data loaded

--- js/script.js
+++ js/script.js
@@ -22,10 +22,35 @@ const showResults = phones => {
                 <div class="card-body">
                     <h5 class="card-title">${phone.phone_name}</h5>
                     <p class="card-text">${phone.brand}</p>
-                    <button class="btn btn-outline-success" type="submit" onclick="">Details</button>
+                    <button class="btn btn-outline-success" type="submit" onclick="detailButton('${phone.slug}')">Details</button>
                 </div>
             </div>
         `
         resultContainer.appendChild(div);
     }
+}
+
+const detailButton = id => {
+    console.log(id)
+    const url = `https://openapi.programming-hero.com/api/phone/${id}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => showDetails(data))
+}
+
+const showDetails = detail => {
+    console.log(detail)
+    const detailContainer = document.getElementById('detail-field');
+    const div = document.createElement('div');
+    div.innerHTML = `
+        <div class="card" style="width: 18rem;">
+            <img src="${detail.data.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${detail.data.name}</h5>
+                <p class="card-text">${detail.data.brand}</p>
+                <p class="card-text">${detail.data.releaseDate}</p>
+            </div>
+        </div>
+    `
+    detailContainer.appendChild(div);
 }
