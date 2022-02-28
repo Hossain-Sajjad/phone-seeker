8834beaf313c706cc968c5e48c09bfa68128053a
Hossain-Sajjad <sunshinems58.com>
2022-03-01 03:09:49 +0600

    loaded api data

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
 const searchButton = () => {
-    console.log("paise");
+    const inputValue = document.getElementById('input-field').value;
+    loadApiResult(inputValue);
+}
+
+const loadApiResult = inputValue => {
+    const url = `https://openapi.programming-hero.com/api/phones?search=${inputValue}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => showResults(data))
+}
+
+const showResults = data => {
+
 }
