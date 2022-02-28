dbb47eea2969169ced1c00d9708141efb54b52c0
Hossain-Sajjad <sunshinems58.com>
2022-03-01 03:31:12 +0600

    showed search result

--- js/script.js
+++ js/script.js
@@ -10,6 +10,22 @@ const loadApiResult = inputValue => {
         .then(data => showResults(data))
 }
 
-const showResults = data => {
-
+const showResults = phones => {
+    console.log(phones)
+    const resultContainer = document.getElementById('result-field');
+    for (const phone of phones.data) {
+        const div = document.createElement('div');
+        div.classList.add('col-lg-4')
+        div.innerHTML = `
+            <div class="card" style="width: 18rem;">
+                <img src="${phone.image}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${phone.phone_name}</h5>
+                    <p class="card-text">${phone.brand}</p>
+                    <button class="btn btn-outline-success" type="submit" onclick="">Details</button>
+                </div>
+            </div>
+        `
+        resultContainer.appendChild(div);
+    }
 }
